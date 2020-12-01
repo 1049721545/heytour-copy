72c9cbe6419e67ebc1f757a7618450697bec129c
Brav0 <50433581+1049721545@users.noreply.github.com>
2020-12-01 17:51:06 +1300

    Updated layout AB#48

--- src/job/JobList.js
+++ src/job/JobList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Image } from "semantic-ui-react";
+import { Card, Image, Button } from "semantic-ui-react";
 import data from "../data/data.json";
 
 function JobList() {
@@ -11,22 +11,25 @@ function JobList() {
         break;
       case "false":
         status = "Not Active";
+        break;
     }
 
     return (
       <Card key={data.id} fluid>
-        <Card.Content>
+        <Card.Content textAlign="left">
           <Card.Header>
-            {data.title} - {data.company} ({status})
+            <Image src={data.picture} size="mini" /> {data.title} -{" "}
+            {data.company} ({status})
           </Card.Header>
         </Card.Content>
-        <Card.Content>
-          <Image src={data.picture} size="mini" />
+        <Card.Content textAlign="left">
           <Card.Meta>{data.location}</Card.Meta>
           <Card.Meta>{data.industry}</Card.Meta>
           <Card.Meta>{data.email}</Card.Meta>
           <Card.Meta>{data.postedOn}</Card.Meta>
           <Card.Description>{data.jobDesc}</Card.Description>
+          <Button icon="star" floated="right" content="Save" />
+          <Button icon="thumbs down" floated="right" content="Not Interested" />
         </Card.Content>
       </Card>
     );
