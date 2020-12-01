167038d788b4f3cf91469082c9467edc7cedaa20
Brav0 <50433581+1049721545@users.noreply.github.com>
2020-12-01 17:37:32 +1300

    Able to retrieve data from the json file can correctly display on screen except isActive AB#48

--- src/job/JobList.js
+++ src/job/JobList.js
@@ -1,8 +1,37 @@
 import React from "react";
-import JobDetail from "./JobDetail";
+import { Card, Image } from "semantic-ui-react";
+import data from "../data/data.json";
 
 function JobList() {
-  return <JobDetail />;
+  const status = "";
+  const getData = data.map((data) => {
+    switch (data.isActive) {
+      case "true":
+        status = "Active";
+        break;
+      case "false":
+        status = "Not Active";
+    }
+
+    return (
+      <Card key={data.id} fluid>
+        <Card.Content>
+          <Card.Header>
+            {data.title} - {data.company} ({status})
+          </Card.Header>
+        </Card.Content>
+        <Card.Content>
+          <Image src={data.picture} size="mini" />
+          <Card.Meta>{data.location}</Card.Meta>
+          <Card.Meta>{data.industry}</Card.Meta>
+          <Card.Meta>{data.email}</Card.Meta>
+          <Card.Meta>{data.postedOn}</Card.Meta>
+          <Card.Description>{data.jobDesc}</Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  });
+  return <Card.Group>{getData}</Card.Group>;
 }
 
 export default JobList;
