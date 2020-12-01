ef07a495ff3dc8a4e3f4646f99a4d52ef1decfac
Brav0 <50433581+1049721545@users.noreply.github.com>
2020-12-01 19:42:13 +1300

    Fixed status not showing correctly AB#48

--- src/job/JobList.js
+++ src/job/JobList.js
@@ -1,32 +1,35 @@
 import React from "react";
-import { Card, Image, Button, Grid } from "semantic-ui-react";
+import { Card, Image, Button, Grid, Icon } from "semantic-ui-react";
 import data from "../data/data.json";
 
 export default function JobList() {
   const status = "";
   const getData = data.map((data) => {
-    switch (data.isActive) {
-      case "true":
-        status = "Active";
-        break;
-      case "false":
-        status = "Not Active";
-        break;
-    }
-
     return (
       <Card key={data.id} fluid>
         <Card.Content textAlign="left">
           <Card.Header>
-            <Image src={data.picture} size="mini" /> {data.title} -{" "}
-            {data.company} ({status})
+            {data.title} - {data.company}
           </Card.Header>
         </Card.Content>
         <Card.Content textAlign="left">
-          <Card.Meta>Location: {data.location}</Card.Meta>
+          <Card.Meta>
+            Status:{" "}
+            {data.isActive ? (
+              <Icon name="circle check" color="green" content="Active">
+                Active
+              </Icon>
+            ) : (
+              <Icon name="times circle" color="red" content="Expired">
+                Expired
+              </Icon>
+            )}
+          </Card.Meta>
           <Card.Meta>Industry: {data.industry}</Card.Meta>
+          <Card.Meta>Location: {data.location}</Card.Meta>
           <Card.Meta>Email: {data.email}</Card.Meta>
           <Card.Meta>Post Date: {data.postedOn}</Card.Meta>
+          <Image src={data.picture} size="small" floated="right" />
         </Card.Content>
         <Card.Content textAlign="left">
           <Card.Description>Description: </Card.Description>
