10486c34ac2eae71ab93211341e9d26c43ba0e3e
Brav0 <50433581+1049721545@users.noreply.github.com>
2020-12-01 19:57:21 +1300

    Last commit AB#48

--- src/job/JobList.js
+++ src/job/JobList.js
@@ -3,57 +3,61 @@ import { Card, Image, Button, Grid, Icon } from "semantic-ui-react";
 import data from "../data/data.json";
 
 export default function JobList() {
-  const status = "";
   const getData = data.map((data) => {
     return (
-      <Card key={data.id} fluid>
-        <Card.Content textAlign="left">
-          <Card.Header>
-            {data.title} - {data.company}
-          </Card.Header>
-        </Card.Content>
-        <Card.Content textAlign="left">
-          <Card.Meta>
-            Status:{" "}
-            {data.isActive ? (
-              <Icon name="circle check" color="green" content="Active">
-                Active
-              </Icon>
-            ) : (
-              <Icon name="times circle" color="red" content="Expired">
-                Expired
-              </Icon>
-            )}
-          </Card.Meta>
-          <Card.Meta>Industry: {data.industry}</Card.Meta>
-          <Card.Meta>Location: {data.location}</Card.Meta>
-          <Card.Meta>Email: {data.email}</Card.Meta>
-          <Card.Meta>Post Date: {data.postedOn}</Card.Meta>
-          <Image src={data.picture} size="small" floated="right" />
-        </Card.Content>
-        <Card.Content textAlign="left">
-          <Card.Description>Description: </Card.Description>
-          <Card.Description>{data.jobDesc}</Card.Description>
-          <Button icon="plus" floated="right" content="Apply" color="green" />
-          <Button icon="star" floated="right" content="Save" />
-          <Button
-            icon="thumbs down"
-            floated="right"
-            content="Not Interested"
-            color="red"
-          />
-        </Card.Content>
-      </Card>
+      <Grid.Row>
+        <Grid.Column>
+          <Card key={data.id} fluid>
+            <Card.Content textAlign="left">
+              <Card.Header>
+                {data.title} - {data.company}
+              </Card.Header>
+            </Card.Content>
+            <Card.Content textAlign="left">
+              <Card.Meta>
+                Status:{" "}
+                {data.isActive ? (
+                  <Icon name="circle check" color="green">
+                    Active
+                  </Icon>
+                ) : (
+                  <Icon name="times circle" color="red">
+                    Expired
+                  </Icon>
+                )}
+              </Card.Meta>
+              <Card.Meta>Industry: {data.industry}</Card.Meta>
+              <Card.Meta>Location: {data.location}</Card.Meta>
+              <Card.Meta>Email: {data.email}</Card.Meta>
+              <Card.Meta>Post Date: {data.postedOn}</Card.Meta>
+              <Image src={data.picture} size="small" floated="right" />
+            </Card.Content>
+            <Card.Content textAlign="left">
+              <Card.Description>Description: </Card.Description>
+              <Card.Description>{data.jobDesc}</Card.Description>
+              <Button
+                icon="plus"
+                floated="right"
+                content="Apply"
+                color="green"
+              />
+              <Button icon="star" floated="right" content="Save" />
+              <Button
+                icon="thumbs down"
+                floated="right"
+                content="Not Interested"
+                color="red"
+              />
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      </Grid.Row>
     );
   });
 
   return (
     <Grid fluid padded>
-      <Grid.Row>
-        <Grid.Column>
-          <Card.Group>{getData}</Card.Group>
-        </Grid.Column>
-      </Grid.Row>
+      {getData}
     </Grid>
   );
 }
